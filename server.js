0037758d0e21ef982a49961ed940fc2ed334d5b0
dk <hackersantochi@gmail.com>
2022-01-03 17:10:26 +0100

    change the name of routers

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv/config");
 const express = require("express");
 const app = express();
-const signUpRouter = require("./routers/Sign-up");
+const Routers = require("./routers/Routers");
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.set("views", [__dirname + "/views", __dirname + "/public"]);
@@ -17,7 +17,7 @@ mongoose.connection.on("connected", function () {
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(signUpRouter);
+app.use(Routers);
 app.get("/", (req, res) => {
   res.render("index");
 });
